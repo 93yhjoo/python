6f43603e40bfe206d21b41392e7a5d5f69af7d87
93yhjoo <gottkrhk@naver.com>
2018-07-17 20:28:22 +0900

    20180717

--- alg/callback_fun.py
+++ alg/callback_fun.py
@@ -1,57 +1,43 @@
 """재귀함수 사용하기"""
 
 def hasWord(x,y,argstart,word):
-    flag=False
     dx=[-1,-1,-1,0,0,1,1,1]
     dy=[-1,0,1,-1,1,-1,0,1]
-    """구현 사항: PRETTY 일 경우 R 접근 경로 2가지"""
+    for d in range(0,7):
+        xi=dx[d]+x
+        yi=dy[d]+y
+        if xi < 0 or yi < 0:
+            continue
+        try:
+            alpha=li[xi][yi]
+            if alpha is word[argstart]:
 
-    """마지막 문자까지 검색하여 다시 재귀해서 들어왔을 시 탈출"""
-    if(argstart>=len(word)):
-        flag=True
-    else:
-        for d in range(len(dx)):
-            xi=x+dx[d]
-            yi=y+dy[d]
-            try:
-                if(xi>= 0 and yi >= 0):
-                    if(li[xi][yi]==word[argstart:argstart+1]):
-                        stack.append([xi,yi])
-                        flag=True
-                        hasWord(xi,yi,argstart+1,word)
-                        break
-                else:
-                    continue
-            except IndexError as e:
+                argstart=1+argstart
+                if argstart is len(word):
+                    print('{}을 찾았습니다.'.format(usr))
+                    return True
+                hasWord(xi,yi,argstart,word)
+
+        except IndexError:
                 continue
-        if flag == False:
-            stack.clear()
+
+
 
 
 
 
 li = [['U','R','L','P','M'],['X','P','R','E','T'],['G','I','A','E','T'],['X','T','N','Z','Y'],['X','O','Q','R','S']]
 
-stack=[]
-start=1
+check=False
 usr=str(input())
-for x in range(len(li)):
-    if(len(usr) == 1):
-        try:
-            y=li[x].index(usr)
-            stack.append([x,y])
-        except ValueError as e:
-            continue
-    else:
-        try:
-            y = li[x].index(usr[:start])
-            stack.append([x, y])
-            hasWord(x,y,start,usr)
-        except ValueError as e:
-            continue
-
-
-if(len(stack)==0):
-    print("존재 하지 않습니다")
-else:
-    print(stack)
+for row in range(0,5):
+    try:
+        col=li[row].index(usr[0])
+        if len(usr) > 1:
+            check=hasWord(row,col,1,usr)
+        else:
+            print('{}을 찾았습니다.'.format(usr))
+    except ValueError:
+        if row is 4 and check is False:
+            print('{}찾지 못했습니다.'.format(usr))
+        continue
---
+++ alg/fence_problem(DNC).py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+def init_bar(fence):
+    x = []
+    y = []
+    for i in range(len(fence)):
+        x.append(i)
+        y.append(fence[i])
+    #plt.figure()
+    plt.bar(x,fence,width=1.0,color='b')
+    #plt.show()
+def solve(left, right):
+    ##one fence
+    if left is right:
+        return fence[left]
+    middle=int((left+right) /2)
+    ##recursive Divice
+    result=max(solve(left,middle),solve(middle+1,right))
+    low=middle
+    high=middle+1
+    height=min(fence[low],fence[high])
+
+    ##높이 책정
+    result=max(result,height*2)
+    while left<low or high<right :
+        if high < right and (low is left or fence[low-1] < fence[high+1]) :
+            high=high+1
+            height=min(height,fence[high])
+        else:
+            low=low-1
+            height=min(height,fence[low])
+        result=max(result,height*(high-low+1))
+    return result
+
+def animate(i):
+    x = []
+    y=[]
+    for j in range(len(fence)):
+        x.append(j)
+        y.append(0)
+
+    return plt.bar(x, y, width=1.0, color='r')
+
+
+fig=plt.figure()
+problem_case=input()
+for i in range(int(problem_case)):
+    num_fence=input()
+    high_fence=input()
+    fence=high_fence.split(" ")
+    fence=list(map(int,fence))
+    ans=solve(0,len(fence)-1)
+    print(ans)
+anim = animation.FuncAnimation(fig, animate, init_func=init_bar(fence), frames=200, interval=1000, blit=True)
+plt.show()
